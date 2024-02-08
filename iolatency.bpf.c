04437dd2dd51a8b6e4809cebd356aedf75af344a
vjabrayilov <vahab.jabrayilov@metu.edu.tr>
2024-02-08 00:04:30 +0000

    polish

--- iolatency.bpf.c
+++ iolatency.bpf.c
@@ -3,9 +3,6 @@
 #include <bpf/bpf_helpers.h>
 #include <bpf/bpf_tracing.h>
 
-// #include "bits.bpf.h"
-// #include "core_fixes.bpf.h"
-
 #define MAX_ENTRIES 10240
 
 #define DISK_NAME_LEN 32
@@ -16,15 +13,12 @@
 
 #define MKDEV(ma, mi) (((ma) << MINORBITS) | (mi))
 
-extern int LINUX_KERNEL_VERSION __kconfig;
-
 const volatile bool filter_dev = false;
 const volatile __u32 targ_dev = 0;
 const volatile bool targ_single = true;
 
 struct hist_key {
-  __u32 cmd_flags;
-  __u32 dev;
+  __u32 key;
 };
 
 struct hist {
@@ -75,37 +69,13 @@ static __always_inline u64 log2l(u64 v) {
     return log2(v);
 }
 
-static int __always_inline trace_rq_start(struct request *rq, int issue) {
+static int __always_inline trace_rq_start(struct request *rq){
   u64 ts;
 
   ts = bpf_ktime_get_ns();
 
   bpf_map_update_elem(&start, &rq, &ts, 0);
   return 0;
-}
-
-static int handle_block_rq_insert(__u64 *ctx) {
-  /**
-   * commit a54895fa (v5.11-rc1) changed tracepoint argument list
-   * from TP_PROTO(struct request_queue *q, struct request *rq)
-   * to TP_PROTO(struct request *rq)
-   */
-  // if (!targ_single)
-  // return trace_rq_start((void *)ctx[1], false);
-  // else
-  return trace_rq_start((void *)ctx[0], false);
-}
-
-static int handle_block_rq_issue(__u64 *ctx) {
-  /**
-   * commit a54895fa (v5.11-rc1) changed tracepoint argument list
-   * from TP_PROTO(struct request_queue *q, struct request *rq)
-   * to TP_PROTO(struct request *rq)
-   */
-  // if (!targ_single)
-  // return trace_rq_start((void *)ctx[1], true);
-  // else
-  return trace_rq_start((void *)ctx[0], true);
 }
 
 static int handle_block_rq_complete(struct request *rq, int error,
@@ -142,23 +112,11 @@ cleanup:
   return 0;
 }
 
-// SEC("tp_btf/block_rq_insert")
-// int block_rq_insert_btf(u64 *ctx) { return handle_block_rq_insert(ctx); }
-
-// SEC("tp_btf/block_rq_issue")
-// int block_rq_issue_btf(u64 *ctx) { return handle_block_rq_issue(ctx); }
-
-// SEC("tp_btf/block_rq_complete")
-// int BPF_PROG(block_rq_complete_btf, struct request *rq, int error,
-//              unsigned int nr_bytes) {
-//   return handle_block_rq_complete(rq, error, nr_bytes);
-// }
-
 SEC("raw_tp/block_rq_insert")
-int BPF_PROG(block_rq_insert) { return handle_block_rq_insert(ctx); }
+int BPF_PROG(block_rq_insert) { return trace_rq_start((void *)ctx[0]); }
 
 SEC("raw_tp/block_rq_issue")
-int BPF_PROG(block_rq_issue) { return handle_block_rq_issue(ctx); }
+int BPF_PROG(block_rq_issue) { return trace_rq_start((void *)ctx[0]); }
 
 SEC("raw_tp/block_rq_complete")
 int BPF_PROG(block_rq_complete, struct request *rq, int error,
--- iolatency.c
+++ iolatency.c
@@ -1,120 +1,10 @@
-#include <bpf/bpf.h>
-#include <bpf/btf.h>
-#include <bpf/libbpf.h>
-#include <bpf/libbpf_legacy.h>
-#include <errno.h>
-#include <fcntl.h>
-#include <signal.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <sys/resource.h>
-#include <time.h>
-#include <unistd.h>
-#define MAX_SLOTS 27
+#include "iolatency.h"
 
-struct hist_key {
-  __u32 cmd_flags;
-  __u32 dev;
-};
-
-struct hist {
-  __u32 slots[MAX_SLOTS];
-};
-
-#define ARRAY_SIZE(x) (sizeof(x) / sizeof(*(x)))
-
-#define min(x, y)                                                              \
-  ({                                                                           \
-    typeof(x) _min1 = (x);                                                     \
-    typeof(y) _min2 = (y);                                                     \
-    (void)(&_min1 == &_min2);                                                  \
-    _min1 < _min2 ? _min1 : _min2;                                             \
-  })
 static volatile bool exiting;
 
-static void print_stars(unsigned int val, unsigned int val_max, int width) {
-  int num_stars, num_spaces, i;
-  bool need_plus;
 
-  num_stars = min(val, val_max) * width / val_max;
-  num_spaces = width - num_stars;
-  need_plus = val > val_max;
-
-  for (i = 0; i < num_stars; i++)
-    printf("*");
-  for (i = 0; i < num_spaces; i++)
-    printf(" ");
-  if (need_plus)
-    printf("+");
-}
-void print_log2_hist(unsigned int *vals, int vals_size, const char *val_type) {
-  int stars_max = 40, idx_max = -1;
-  unsigned int val, val_max = 0;
-  unsigned long long low, high;
-  int stars, width, i;
-
-  for (i = 0; i < vals_size; i++) {
-    val = vals[i];
-    if (val > 0)
-      idx_max = i;
-    if (val > val_max)
-      val_max = val;
-  }
-
-  if (idx_max < 0)
-    return;
-
-  printf("%*s%-*s : count    distribution\n", idx_max <= 32 ? 5 : 15, "",
-         idx_max <= 32 ? 19 : 29, val_type);
-
-  if (idx_max <= 32)
-    stars = stars_max;
-  else
-    stars = stars_max / 2;
-
-  for (i = 0; i <= idx_max; i++) {
-    low = (1ULL << (i + 1)) >> 1;
-    high = (1ULL << (i + 1)) - 1;
-    if (low == high)
-      low -= 1;
-    val = vals[i];
-    width = idx_max <= 32 ? 10 : 20;
-    printf("%*lld -> %-*lld : %-8d |", width, low, width, high, val);
-    print_stars(val, val_max, stars);
-    printf("|\n");
-  }
-}
 static void sig_handler(int sig) { exiting = true; }
 
-static int print_log2_hists(struct bpf_map *hists) {
-  struct hist_key lookup_key = {.cmd_flags = -1}, next_key;
-  const char *units = "usecs";
-  int err, fd = bpf_map__fd(hists);
-  struct hist hist;
-
-  while (!bpf_map_get_next_key(fd, &lookup_key, &next_key)) {
-    err = bpf_map_lookup_elem(fd, &next_key, &hist);
-    if (err < 0) {
-      fprintf(stderr, "failed to lookup hist: %d\n", err);
-      return -1;
-    }
-    print_log2_hist(hist.slots, MAX_SLOTS, units);
-    lookup_key = next_key;
-  }
-
-  lookup_key.cmd_flags = -1;
-  while (!bpf_map_get_next_key(fd, &lookup_key, &next_key)) {
-    err = bpf_map_delete_elem(fd, &next_key);
-    if (err < 0) {
-      fprintf(stderr, "failed to cleanup hist : %d\n", err);
-      return -1;
-    }
-    lookup_key = next_key;
-  }
-
-  return 0;
-}
 
 int main(int argc, char **argv) {
   struct bpf_object *obj;
@@ -170,43 +60,6 @@ int main(int argc, char **argv) {
       return 1;
     }
   }
-  // const char *tracepoints[] = {"block_rq_insert", "block_rq_issue",
-  //                              "block_rq_complete"};
-
-  // for (size_t i = 0; i < 3; i++) {
-  //   // Attach BPF program
-  //   fprintf(stderr, "Attaching BPF program to tracepoint\n");
-  //   prog = bpf_object__find_program_by_name(obj, tracepoints[i]);
-  //   if (libbpf_get_error(prog)) {
-  //     fprintf(stderr, "ERROR: finding BPF program failed\n");
-  //     return 1;
-  //   }
-  //   prog_fd = bpf_program__fd(prog);
-  //   if (prog_fd < 0) {
-  //     fprintf(stderr, "ERROR: getting BPF program FD failed\n");
-  //     return 1;
-  //   }
-  //   // Load BPF program
-  //   fprintf(stderr, "Loading and verifying the code in the kernel\n");
-  //   if (bpf_object__load(obj)) {
-  //     fprintf(stderr, "ERROR: loading BPF object file failed\n");
-  //     return 1;
-  //   }
-
-  //   // Attach
-
-  //   // for (size_t i = 0; i < sizeof(tracepoints) / sizeof(tracepoints[0]);
-  //   ++i)
-  //   // {
-
-  //   links[i] = bpf_program__attach_tracepoint(prog, "block", tracepoints[i]);
-  //   if (libbpf_get_error(links[i])) {
-  //     fprintf(stderr, "ERROR: Attaching BPF program to tracepoint %s
-  //     failed\n",
-  //             tracepoints[i]);
-  //     return 1;
-  //   }
-  // }
 
   signal(SIGINT, sig_handler);
 
@@ -222,8 +75,6 @@ int main(int argc, char **argv) {
   /* main: poll */
   while (1) {
     sleep(interval);
-    // TODO: fix map and pass to the function below
-
     err = print_log2_hists(map);
     if (err)
       break;
